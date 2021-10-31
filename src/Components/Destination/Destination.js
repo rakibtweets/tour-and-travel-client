5d0a33d3bd060f8f7139442943a23143432a84a5
rakibtweets <rakibhasanroky0@gmail.com>
2021-10-31 18:56:12 +0600

    destination animation done

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Destination.css';
 import { Card, Col, Container, Row, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Fade from 'react-reveal/Fade';
 
 const Destination = () => {
   const [destinations, setDestinations] = useState([]);
@@ -19,47 +20,51 @@ const Destination = () => {
       <Container>
         <Row xs={1} md={3} lg={3} className=" g-4">
           {destinations.map((destination) => (
-            <Col key={destination?._id} className=" d-flex">
-              <Card className="destination-card">
-                <Card.Img
-                  className=" img-fluid place-img"
-                  variant="top"
-                  src={destination?.placeImg}
-                />
-                <img
-                  className="country-img img-fluid text-end"
-                  src={destination?.countryImg}
-                  alt=""
-                />
+            <Col key={destination?._id} className=" d-block d-flex">
+              <Fade bottom cascade>
+                <div className=" d-block d-flex">
+                  <Card className="destination-card">
+                    <Card.Img
+                      className=" img-fluid place-img"
+                      variant="top"
+                      src={destination?.placeImg}
+                    />
+                    <img
+                      className="country-img img-fluid text-end"
+                      src={destination?.countryImg}
+                      alt=""
+                    />
 
-                <Card.Body className="body-card">
-                  <h5 className="text-success fw-bloder">
-                    {destination?.placeName}
-                  </h5>
-                  <p className=" text-secondary p-0">
-                    {destination?.placeDescription.slice(0, 120)}
-                  </p>
-                  <div className=" d-flex justify-content-between text-success fw-bold">
-                    <h5>{destination?.travelDuration} days</h5>
-                    <h5>$ {destination?.pricing}</h5>
-                  </div>
-                </Card.Body>
-                <div className="d-flex justify-content-around body-card">
-                  <Link to={`/destinations/${destination?._id}`}>
-                    <Button className=" destination-body" variant="success">
-                      Details
-                    </Button>
-                  </Link>
-                  <Link to={`/booking/${destination?._id}`}>
-                    <Button
-                      className=" destination-body px-3"
-                      variant="outline-success"
-                    >
-                      Book Now
-                    </Button>
-                  </Link>
+                    <Card.Body className="body-card">
+                      <h5 className="text-success fw-bloder">
+                        {destination?.placeName}
+                      </h5>
+                      <p className=" text-secondary p-0">
+                        {destination?.placeDescription.slice(0, 120)}
+                      </p>
+                      <div className=" d-flex justify-content-between text-success fw-bold">
+                        <h5>{destination?.travelDuration} days</h5>
+                        <h5>$ {destination?.pricing}</h5>
+                      </div>
+                    </Card.Body>
+                    <div className="d-flex justify-content-around body-card">
+                      <Link to={`/destinations/${destination?._id}`}>
+                        <Button className=" destination-body" variant="success">
+                          Details
+                        </Button>
+                      </Link>
+                      <Link to={`/booking/${destination?._id}`}>
+                        <Button
+                          className=" destination-body px-3"
+                          variant="outline-success"
+                        >
+                          Book Now
+                        </Button>
+                      </Link>
+                    </div>
+                  </Card>
                 </div>
-              </Card>
+              </Fade>
             </Col>
           ))}
         </Row>
